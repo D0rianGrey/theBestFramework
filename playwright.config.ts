764ef6ed5769bf073e3a6e750ead8ce75069799c
Евгений Вакерин <utyfdfrthby08@gmail.com>
2025-06-20 13:28:07 +0200

    Enable headless mode for tests in CI environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: false,
+
+    /* Run tests in headless mode */
+    headless: process.env.CI ? true : false,
   },
 
   /* Configure projects for major browsers */
